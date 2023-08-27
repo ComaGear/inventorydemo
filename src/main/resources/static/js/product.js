ebc44c1bb059e71d776b675c9f495838f1721f3e
Coma Gear <coma.gear921@gmail.com>
2023-08-27 23:34:46 +0800

    selection of measurement

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -1,28 +1,13 @@
+// @ts-nocheck
 const currentUrl = window.location.host;
-console.log(currentUrl);
 
-async function post(boby){
-    const url = "http://" + currentUrl + "/product/";
-    const response = await fetch(url, {
-        method : "POST",
-        body: boby,
-        headers : {
-            "Content-Type": "application/json"
-        },
-    });
+async function createListener(){
 
-    return response.json();
-}
+    updateMeasure();
 
-let createButton = document.querySelector("button#create");
-createButton?.addEventListener("click", ()=>{
-    // @ts-ignore
     let product_id = document.querySelector("#product_id")?.value;
-    // @ts-ignore
     let product_name = document.querySelector("#product_name")?.value;
-    // @ts-ignore
     let product_default_uom = document.querySelector("#product_uom")?.value;
-    // @ts-ignore
     let active = document.querySelector("#active_yes")?.checked;
 
     const boby = JSON.stringify({
@@ -34,8 +19,43 @@ createButton?.addEventListener("click", ()=>{
         }
     });
 
-    post(boby).then((data) =>{
+    const url = "http://" + currentUrl + "/product/";
+    const response = await fetch(url, {
+        method : "POST",
+        body: boby,
+        headers : {
+            "Content-Type": "application/json"
+        },
+    });
+
+    response.json().then((data) =>{
         console.log(data)
     });
-})
+}
+
+async function selectOnChange(){
+    let select_list = document.querySelector("select#measurement_list");
+
+    if(select_list.options[select_list.selectedIndex].text == "new Measure"){
+        let measurement_name = document.querySelector("measurement_name");
+        let measure = document.querySelector("measure");
+        let barcode = document.querySelector("barcode");
+
+        measurement_name.value = "";
+        measure.value = "";
+        barcode.value = "";
+    }
+    
+    // select_list?
+}
+
+async function updateMeasure(){
+    // TODO handle job about post a new measurement check <option> 'add Meas' is valid to post.
+    // or update some measuremnt with put
+}
+
+let createButton = document.querySelector("button#create");
+createButton?.addEventListener("click", createListener);
+let measurement_list = document.querySelector("#measurement_list");
+measurement_list?.addEventListener("change", selectOnChange);
 
